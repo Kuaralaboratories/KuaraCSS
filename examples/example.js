ac643813103c5a98ea95121658b5a37f292781ca
yakupcemilk <kayabasyakupcemil@gmail.com>
2024-07-30 11:19:06 +0300

    Example added

---
+++ examples/example.js
@@ -0,0 +1,20 @@
+import kuaracss from "kuaracss";
+
+const styles = kuaracss.create({
+  blue: {
+    color: "blue",
+  },
+  red: {
+    color: "red",
+  },
+});
+
+const isRed = true; // Example condition
+
+document.querySelectorAll("h1").forEach((el) => {
+  el.className = styles("blue", isRed && "red");
+});
+
+document.querySelectorAll("h2").forEach((el) => {
+  el.className = styles("blue", isRed && "red");
+});
